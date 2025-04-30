ef5c11a773384cd3a9308f98a58b8e5ebbd285de
ghineaal <alin.ghinea@kronsoft.ro>
2025-04-30 16:58:09 +0300

    add ssr-output and adapter move welcome page to index.astro fix title as props

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   integrations: [solid()],
 });
